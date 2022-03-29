24d681ba3c46ba3421fe9bec4681609aad6ed3be
skshabeera <82590734+skshabeera@users.noreply.github.com>
2022-03-29 12:37:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ connectDB()
 app.use(express.json({ extended:false }))
 
 app.get('/',(req,res)=>res.send('API running'))
-app.use("/api/auth",require("./routes/api/auth"))
 app.use("/api/user",require("./routes/api/user"))
 
 
